3ead003a858230f21d3ca288c9f64d127558ec84
Oleg Mayurchenko <vfxq@rambler.com>
2019-02-19 20:33:07 +0300

    added tasks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import uuidv1 from 'uuid/v1';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      newTaskTitle: null,
+      tasks: []
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      newTaskTitle: event.target.value
+    });
+  }
+
+  addTaskHandle = () => {
+    const task = {
+      title: this.state.newTaskTitle, 
+      id: uuidv1()
+    };
+    
+    this.setState({ 
+      tasks: [...this.state.tasks, task]
+    });
+  }
+
   render() {
+    const body = this.state.tasks.length > 0 
+      ?
+        <ul>
+          {
+            this.state.tasks.map(item => 
+              <li key={item.id}>
+               {item.title}
+              </li>
+            )
+          }
+        </ul> 
+      : null
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>ToDo   list</h1>
+        <input type="text" value={this.state.newTatsTitle} onChange={this.handleChange}/>
+        <button onClick={this.addTaskHandle}>Add task</button> 
+        {body}
       </div>
     );
   }
